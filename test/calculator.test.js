ab12191fcdb9622e3c9d6e170bffeb71b5e2beb2
Himanshu Bajpai <himanshu.bajpai@mobileprogramming.com>
2024-06-14 10:34:33 +0530

    created two numbers in a string test case

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -9,4 +9,9 @@ describe('String Calculator', () => {
     test('should return the number itself if only one number is provided', () => {
         expect(calculator.add("1")).toBe(1);
     });
+
+    test('should return the sum of two numbers', () => {
+        expect(add("1,2")).toBe(3);
+        expect(add("10,5")).toBe(15);
+    });
 });
