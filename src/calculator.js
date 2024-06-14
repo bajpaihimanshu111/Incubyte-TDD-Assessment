64a0518779464175fd34740c9aff513441547b3a
Himanshu Bajpai <himanshu.bajpai@mobileprogramming.com>
2024-06-14 13:17:45 +0530

    handled delimiter test case in add method

--- src/calculator.js
+++ src/calculator.js
@@ -4,11 +4,19 @@ class Calculator {
         if (numbers === "") {
             return 0;
         }
+        
+        let delimiter = ",";
+        // Check if the input starts with a custom delimiter definition
+        if (numbers.startsWith("//")) {
+            const delimiterIndex = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterIndex);
+            numbers = numbers.substring(delimiterIndex + 1);
+        }
     
-        // Split the input string based on commas and new lines
-        const numberArray = numbers.split(/,|\n/).map(Number);
+        // Split the input string based on the custom delimiter
+        const numberArray = numbers.split(new RegExp(`${delimiter}|\n`)).map(Number);
         return numberArray.reduce((sum, number) => sum + number, 0);
     }
-  }
-  
-  module.exports = Calculator;
+}
+
+module.exports = Calculator;
