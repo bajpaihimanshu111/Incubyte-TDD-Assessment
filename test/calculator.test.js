d5c2da6b5eb2f6c0e79fcdb5b265c4c8db12082b
Himanshu Bajpai <himanshu.bajpai@mobileprogramming.com>
2024-06-14 10:30:49 +0530

    created single number in a string test case

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -5,4 +5,8 @@ describe('String Calculator', () => {
     test('should return 0 for an empty string', () => {
         expect(calculator.add("")).toBe(0);
     });
+
+    test('should return the number itself if only one number is provided', () => {
+        expect(calculator.add("1")).toBe(1);
+    });
 });
